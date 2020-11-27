72b288b7f3c3592770edd0dac8c9e03fd084f895
Daniele Megna <megna.dany@gmail.com>
2020-11-27 21:30:44 +0100

    Step back refactoring: explode items map operation
    Explode ShoppingBasket.items > Receipt.items map to simple foreach
    with mutable list and mutable total salesTaxes.
    Temporary less elegant solution to explore new tax calculation solutions.

--- src/main/kotlin/it/dmegna/ksalestaxes/CashRegister.kt
+++ src/main/kotlin/it/dmegna/ksalestaxes/CashRegister.kt
@@ -3,15 +3,22 @@ package it.dmegna.ksalestaxes
 class CashRegister {
 
     fun receiptFor(shoppingBasket: ShoppingBasket): Receipt {
-        return Receipt(
-            items = receiptItemsFrom(shoppingBasket),
-            salesTaxes = 0.0
-        )
+        val items = mutableListOf<Receipt.Item>()
+        var salesTaxes = 0.0
+
+        shoppingBasket.items.forEach {
+            val unitItemTaxes = calculateUnitItemTaxesFor(it)
+            val taxedUnitPrice = it.unitNetPrice + unitItemTaxes
+            val taxedTotalPrice = taxedUnitPrice * it.qty
+            items.add(Receipt.Item(it.qty, it.description, taxedTotalPrice))
+            salesTaxes += unitItemTaxes * it.qty
+        }
+
+        return Receipt(items, salesTaxes)
     }
 
-    private fun receiptItemsFrom(shoppingBasket: ShoppingBasket): List<Receipt.Item> {
-        return shoppingBasket.items.map {
-            Receipt.Item(it.qty, it.description, it.unitNetPrice * it.qty)
-        }
+    private fun calculateUnitItemTaxesFor(it: ShoppingBasket.Item): Double {
+        return 0.0
     }
+
 }
