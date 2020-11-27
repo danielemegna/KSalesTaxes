c035fee9a76a755d52045f5cda25bb8c4632d431
Daniele Megna <megna.dany@gmail.com>
2020-11-27 19:29:33 +0100

    Minimal step: handle multiple quantity in simple untaxed shopping

--- src/test/kotlin/it/dmegna/ksalestaxes/acceptance/SimpleShoppingAcceptanceTest.kt
+++ src/test/kotlin/it/dmegna/ksalestaxes/acceptance/SimpleShoppingAcceptanceTest.kt
@@ -8,19 +8,40 @@ import org.junit.Test
 
 class SimpleShoppingAcceptanceTest {
 
+    private val cashRegister = CashRegister()
+
     @Test
     fun `working with single untaxed products`() {
         val shoppingBasket = ShoppingBasket()
-        val cashRegister = CashRegister()
-
         shoppingBasket.add(qty = 1, description = "book", unitPrice = 12.49)
         shoppingBasket.add(qty = 1, description = "chocolate bar", unitPrice = 0.85)
+
         val actualReceipt = cashRegister.receiptFor(shoppingBasket)
 
         val expectedReceipt = Receipt(
             items = listOf(
                 Receipt.Item(1, "book", 12.49),
                 Receipt.Item(1, "chocolate bar", 0.85)
+            ),
+            salesTaxes = 0.0
+        )
+        assertEquals(expectedReceipt, actualReceipt)
+    }
+
+    @Test
+    fun `working with multiple untaxed products`() {
+        val shoppingBasket = ShoppingBasket()
+        shoppingBasket.add(qty = 1, description = "book", unitPrice = 12.49)
+        shoppingBasket.add(qty = 3, description = "chocolate bar", unitPrice = 0.85)
+        shoppingBasket.add(qty = 2, description = "packet of headache pills", unitPrice = 9.75)
+
+        val actualReceipt = cashRegister.receiptFor(shoppingBasket)
+
+        val expectedReceipt = Receipt(
+            items = listOf(
+                Receipt.Item(1, "book", 12.49),
+                Receipt.Item(3, "chocolate bar", 0.85 * 3),
+                Receipt.Item(2, "packet of headache pills", 9.75 * 2)
             ),
             salesTaxes = 0.0
         )
